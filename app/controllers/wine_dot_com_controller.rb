325427b38a27e81ae5b9eb5371a57c58660b087d
John Richardson <richardsonjm@gmail.com>
2016-04-19 15:18:31 -0400

    Add dump catalog button

--- app/controllers/wine_dot_com_controller.rb
+++ app/controllers/wine_dot_com_controller.rb
@@ -2,4 +2,8 @@ class WineDotComController < ApplicationController
   def load_catalog
     WineDotCom.new(params[:records].to_i).load_catalog
   end
+
+  def dump_catalog
+    Wine.where(:api_id.ne => nil).destroy_all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :wines
   root 'wines#index'
   get 'load_catalog', to: 'wine_dot_com#load_catalog'
+  delete 'dump_catalog', to: 'wine_dot_com#dump_catalog'
   get 'dashboard', to: 'pages#dashboard'
 end
