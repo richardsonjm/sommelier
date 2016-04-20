21809863ef2b01a33ce3d063eb60bae24d1159e9
John Richardson <richardsonjm@gmail.com>
2016-04-20 09:15:17 -0400

    Destroy associated records with dump catalog

--- app/controllers/wine_dot_com_controller.rb
+++ app/controllers/wine_dot_com_controller.rb
@@ -6,6 +6,8 @@ class WineDotComController < ApplicationController
 
   def dump_catalog
     Wine.where(:api_id.ne => nil).destroy_all
+    Appellation.destroy_all
+    Varietal.destroy_all
     @notice = "Wine.com records successfully removed from database."
   end
 end
--- spec/controllers/wines_dot_com_controller_spec.rb
+++ spec/controllers/wines_dot_com_controller_spec.rb
@@ -7,4 +7,22 @@ RSpec.describe WineDotComController, type: :controller do
       xhr :get, :load_catalog, format: :js
     end
   end
+
+  describe "DELETE #dump_catalog" do
+    it "calls dump_catalog a an instance of WineDotCom" do
+      expect(Wine).to receive_message_chain('where.destroy_all').and_return('foo')
+      expect(Appellation).to receive(:destroy_all).and_return('bar')
+      expect(Varietal).to receive(:destroy_all).and_return('baz')
+      xhr :delete, :dump_catalog, format: :js
+    end
+
+    it 'leaves user created records in the database' do
+      expect {
+        Wine.create(name: 'foo', price_retail: '2.99')
+      }.to change(Wine, :count).by(1)
+      expect {
+        xhr :delete, :dump_catalog, format: :js
+      }.not_to change(Wine, :count)
+    end
+  end
 end
